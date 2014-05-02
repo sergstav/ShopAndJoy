fdfb79441508fd17a4c09bf828638cb9456c9a77
Sergey Stavitckii <serg@Sergeys-MacBook-Air.local>
2014-05-03 00:34:47 +0300

    Desing fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  validates :title, :category, :description, :image_url, presence: true
+
+
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  validates :image_url, allow_blank: true, format:
+  {
+      with: %r{\.(gif|jpg|png)}i,
+      message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 Product.delete_all
 
-Product.create(title: 'Apple Iphone5s', category: 'smartphones', description: %{Под внешним дизайном iPhone 5S скрывается
+Product.create(title: 'Apple Iphone5s', category: 'smartphones', description: %{<p>Под внешним дизайном iPhone 5S скрывается
 много новых возможностей. Улучшилось качество сенсора, стекло стало на много крепче и более стойкое к следам отпечатков пальцев.
 Так же 2 новыйх процессора A7 64битный и графический M7.
-Улучшенная камера и теперь поддержка всех LTE сетей. Новая операционная система iOS 7.0}, image_url: 'iphone5s.jpg', amount: 47, price: 1010)
+Улучшенная камера и теперь поддержка всех LTE сетей. Новая операционная система iOS 7.0</p>}, image_url: 'iphone5s.jpg', amount: 47, price: 1010)
 
-Product.create(title: 'Apple Iphone5s', category: 'smartphones', description: %{Под внешним дизайном iPhone 5S скрывается
+Product.create(title: 'Apple Iphone5s', category: 'smartphones', description: %{<p>Под внешним дизайном iPhone 5S скрывается
 много новых возможностей. Улучшилось качество сенсора, стекло стало на много крепче и более стойкое к следам отпечатков пальцев.
 Так же 2 новыйх процессора A7 64битный и графический M7.
-Улучшенная камера и теперь поддержка всех LTE сетей. Новая операционная система iOS 7.0},image_url: 'iphone5sblack.jpg', amount: 50, price: 800)
+Улучшенная камера и теперь поддержка всех LTE сетей. Новая операционная система iOS 7.0</p>},image_url: 'iphone5sblack.jpg', amount: 50, price: 800)
 
-Product.create(title: 'Apple MacBookPro Retina', category: 'notebooks', description: %{Дисплей Retina (от англ. "сетчатка") был впервые использован в смартфонах iPhone 4,
+Product.create(title: 'Apple MacBookPro Retina', category: 'notebooks', description: %{<p>Дисплей Retina (от англ. "сетчатка") был впервые использован в смартфонах iPhone 4,
 вышедших в июне 2011 года.
 Такое название было выбрано Стивом Джобсом потому, что человеческий глаз при высоком разрешении (более 300 точек на дюйм для небольшого экрана смартфона) не способен
 различить отдельные пиксели,
 если держать экран на среднем расстоянии от глаз.
-Производят эти дисплеи для Apple LG Displays и Samsung Electronics}, image_url: 'macbookproretina.jpg', amount: 45, price: 3100)
+Производят эти дисплеи для Apple LG Displays и Samsung Electronics</p>}, image_url: 'macbookproretina.jpg', amount: 45, price: 3100)
 
-Product.create(title: 'Apple Macbook Air', category: 'notebooks', description: %{Последнее поколение Mac OS X, процессор Intel Core i5,
+Product.create(title: 'Apple Macbook Air', category: 'notebooks', description: %{<p>Последнее поколение Mac OS X, процессор Intel Core i5,
 высокоскоростной порт
 Thunderbolt, 128 ГБ Flash-накопитель.
- Новый 11-дюймовый MacBook Air - пиковая производительность в портативном исполнении}, image_url: 'macbook_air.jpg', amount: 40, price: 1800)
+ Новый 11-дюймовый MacBook Air - пиковая производительность в портативном исполнении</p>}, image_url: 'macbook_air.jpg', amount: 40, price: 1800)
