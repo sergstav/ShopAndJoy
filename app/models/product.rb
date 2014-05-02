df66e424306ee397b6a03e4702cd58ce76e874ac
Sergey Stavitckii <serg@Sergeys-MacBook-Air.local>
2014-05-03 01:25:48 +0300

    Total price

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,19 @@ class Product < ActiveRecord::Base
       message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
   }
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502215201) do
+ActiveRecord::Schema.define(version: 20140502221513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20140502215201) do
   create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "products", force: true do |t|
